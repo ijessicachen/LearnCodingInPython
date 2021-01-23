a09802aa65101861c68720e478fba41216a6bf1d
egg678 <jessica.chen@leocorn.com>
2021-01-23 01:06:39 +0000

    changed press any key to exit so it works and then changed "any key" to "exit"

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ print("Welcome to my greeting program!")
 name = input("What is your name? ")
 
 print("Hi " + name + ", nice to meet you!")
-print("Press any key to exit")
+input("Press enter to exit ")
