63b915a26d5c176e2117ae03c65844f7cd35b609
egg678 <jessica.chen@leocorn.com>
2021-02-03 20:34:47 +0000

    added thing that kind of changes as you change the size of the screen?

--- L2/CursesBasics/Keyboard.py
+++ L2/CursesBasics/Keyboard.py
@@ -2,10 +2,10 @@
 import curses
 
 def window(keyboard):
-  sh, sw = keyboard.getmaxyx()
-  keyboard.addstr(sh-1, 0, str(sh))
-  keyboard.addstr(sh-1, sw-3, str(sw))
   while True:
+    sh, sw = keyboard.getmaxyx()
+    keyboard.addstr(sh-3, 0, str(sh))
+    keyboard.addstr(sh-3, sw-3, str(sw))
     userKey = keyboard.getch()
     #Get rid of the weird 5 that shows up for 2 digit codes.For this just add a space at taht spot before htis, you'll get it.
     #to make sure it is centred based on if the width is even or odd
@@ -15,8 +15,11 @@ def window(keyboard):
       s = 1
     keyboard.addstr(sh//2, int((sw+1)//2+2+s), " ")
     #Printing the code and the chracter
-    keyboard.addstr(sh//2, int((sw+1)//2+s), str(userKey))  
+    keyboard.addstr(sh//2, int((sw+1)//2+s), str(userKey))
     keyboard.addstr(sh//2, int((sw+1)//2-s), chr(userKey))
     if userKey == 27:
+      #curses.flash()
+      #Not sure if I using this right. Maybe it only wokrs with colour?
       break
 curses.wrapper(window)
+curses.beep()
