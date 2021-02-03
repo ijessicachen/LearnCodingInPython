9c355221cfdc41f9ebc41843b8fc317d9b4b2bc4
egg678 <jessica.chen@leocorn.com>
2021-02-03 13:00:55 +0000

    Added basically all the stuff I had for the keyboard challenge to a new file

--- L2/CursesBasics/Keyboard.py
+++ L2/CursesBasics/Keyboard.py
@@ -1,1 +1,12 @@
 #move the keyboard stuff from above here
+import curses
+
+def window(keyboard):
+  sh, sw = keyboard.getmaxyx()
+  while True:
+    userKey = keyboard.getch()
+    keyboard.addstr(str(userKey))
+    keyboard.addstr(chr(userKey))
+    if userKey == 27:
+      break
+curses.wrapper(window)
