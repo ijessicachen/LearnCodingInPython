5e02f1764d7719efdbeb88a61ab3437b4a48249b
egg678 <jessica.chen@leocorn.com>
2021-02-03 19:29:42 +0000

    So it is centred no matter if the width is even or odd and there's now a space printed on the third digit of the code before the numbers are printed so two digit numbers will be two digits.

--- L2/CursesBasics/Keyboard.py
+++ L2/CursesBasics/Keyboard.py
@@ -3,10 +3,20 @@ import curses
 
 def window(keyboard):
   sh, sw = keyboard.getmaxyx()
+  keyboard.addstr(sh-1, 0, str(sh))
+  keyboard.addstr(sh-1, sw-3, str(sw))
   while True:
     userKey = keyboard.getch()
-    keyboard.addstr(str(userKey))
-    keyboard.addstr(chr(userKey))
+    #Get rid of the weird 5 that shows up for 2 digit codes.For this just add a space at taht spot before htis, you'll get it.
+    #to make sure it is centred based on if the width is even or odd
+    if sw%2 == 0:
+      s = 0.5
+    else:
+      s = 1
+    keyboard.addstr(sh//2, int((sw+1)//2+2+s), " ")
+    #Printing the code and the chracter
+    keyboard.addstr(sh//2, int((sw+1)//2+s), str(userKey))  
+    keyboard.addstr(sh//2, int((sw+1)//2-s), chr(userKey))
     if userKey == 27:
       break
 curses.wrapper(window)
